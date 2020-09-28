44ddee63428d88fa097bb1809cd2ad43c6c30717
Bernt Leander Vargan <b.leandervargan@gmail.com>
2020-09-28 21:12:44 +0200

    Working game Slight bugs in win calculations

--- Compulsory 1/Compulsory 1.cpp
+++ Compulsory 1/Compulsory 1.cpp
@@ -4,47 +4,62 @@
 
 //std::string row1{ "| A | B | C |\n" };      
 //std::string row2{ "| D | E | F |\n" };      // The characters we wish to change here are index 2, 6 & 10
-//std::string row3{ "| G | H | I |\n" };
+//std::string row3{ "| G | H | I |\n" };      // Old system
+void menu();
+
+std::vector <char> board{ '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+std::vector <char> takenSquares{ '0', '0', '0', '0', '0', '0', '0', '0', '0'};
+int takenSquaresCounter{ 0 };
+char input{};
 
 int getInput() {
-    char input{};
 
     std::cin >> input;
 
+    for (int a{ 0 }; a < takenSquaresCounter; a++) {
+        if (input == takenSquares[a]) {
+            std::cout << "Square is already taken!\n";
+            system("pause");
+            return 0;
+        }
+    }
+    takenSquares[takenSquaresCounter] = input;
+    takenSquaresCounter++;
+
     switch (input) {        // Each letter on the grid is given a specific value so that the grid becomes a "magic square"
-    case 'A': case 'a':     // A magic square is an X*X grid (in this case 3*3) where each "tile" is assigned a value so that each row, coloumn and diagonal will sum up to the same value
+    case '1':               // A magic square is an X*X grid (in this case 3*3) where each "tile" is assigned a value so that each row, coloumn and diagonal will sum up to the same value
         return 2;           // This grid looks like this:
         break;              // A(2) B(7) C(6)
                             // D(9) E(5) F(1)
-    case 'B': case 'b':     // G(4) H(3) I(8)
+    case '2':               // G(4) H(3) I(8)
         return 7;           // When the grid is arranged in this way, any row, coloumn or diagonal added together sums up to 15
         break;              // This system makes it so you simply store the tiles assigned value in an array for the player and then add them together
                             // If the sum of 3 array values is 15 then that player has won
-    case 'C': case 'c':
+    case '3':
         return 6;
         break;
 
-    case 'D': case 'd':
+    case '4':
         return 9;
         break;
 
-    case 'E': case 'e':
+    case '5':
         return 5;
         break;
 
-    case 'F': case 'f':
+    case '6':
         return 1;
         break;
 
-    case 'G': case 'g':
+    case '7':
         return 4;
         break;
 
-    case 'H': case 'h':
+    case '8':
         return 3;
         break;
 
-    case 'I': case 'i':
+    case '9':
         return 8;
         break;
 
@@ -55,7 +70,6 @@ int getInput() {
 }
 
 void drawGrid() {
-    std::vector <char> board{ '1', '2', '3', '4', '5', '6', '7', '8', '9' };
     std::string space{ "\t\t\t\t\t\t     " };
 
     std::cout << "\n\n" << space << "---------------\n";
@@ -70,88 +84,53 @@ void drawGrid() {
 
 }
 
-//void swapGrid(int num, char playerID) {
-//    switch (num) {
-//    case 1:
-//        row1[2] = playerID;
-//        break;
-//
-//    case 7:
-//        row1[6] = playerID;
-//        break;
-//
-//    case 6:
-//        row1[10] = playerID;
-//        break;
-//
-//    case 9:
-//        row2[2] = playerID;
-//        break;
-//
-//    case 5:
-//        row2[6] = playerID;
-//        break;
-//
-//    case 1:
-//        row2[10] = playerID;
-//        break;
-//
-//    case 4:
-//        row3[2] = playerID;
-//        break;
-//
-//    case 3:
-//        row3[6] = playerID;
-//        break;
-//
-//    case 8:
-//        row3[10] = playerID;
-//        break;
-//    }
-//}
-
 void swapGrid(int index, char playerID) {
-    switch (index) {
-        
+    if (index > 47) {
+        index -= 48;
     }
+    board[(index - 1)] = playerID;
 }
 
-int calculateWin(int inArray[5]) {          // Checking to see if a player has won by running calculations on the hidden values they have "taken" from the tiles
-    for (int j{ 0 }; j < 2; j++) {
-        for (int k{ 1 }; k < 3; k++) {
-            for (int l{ 2 }; l < 4; l++) {
-                std::cout << inArray[j] + inArray[k] + inArray[l];
-                system("pause");
+void win(int player) {
+    std::cout << "Player " << player << " has won!\n";
+    system("pause");
+    menu();
+}
+
+void calculateWin(int inArray[5], int player) {          // Checking to see if a player has won by running calculations on the hidden values they have "taken" from the tiles
+    for (int j{ 0 }; j < 3; j++) {
+        for (int k{ 1 }; k < 4; k++) {
+            for (int l{ 2 }; l < 5; l++) {
                 if ((inArray[j] + inArray[k] + inArray[l]) == 15) {
-                    return 15;
+                    win(player);
                 }
             }
         }
     }
-    return -1;
+    return;
 }
 
 void game() {
-    /*swapGrid(2, 'A');
-    swapGrid(7, 'B');
-    swapGrid(6, 'C');
-    swapGrid(9, 'D');
-    swapGrid(5, 'E');
-    swapGrid(1, 'F');
-    swapGrid(4, 'G');
-    swapGrid(3, 'H');
-    swapGrid(8, 'I');*/
+    swapGrid(1, '1');
+    swapGrid(2, '2');
+    swapGrid(3, '3');
+    swapGrid(4, '4');
+    swapGrid(5, '5');
+    swapGrid(6, '6');
+    swapGrid(7, '7');
+    swapGrid(8, '8');
+    swapGrid(9, '9');
 
+    takenSquaresCounter = 0;
     drawGrid();
 
     bool validInput = 1;
-    int player1[5]{ 10, 10, 10, 10, 10 };
-    int player2[4]{ 10, 10, 10, 10 };
+    int player1[5]{ 15, 15, 15, 15, 15 };
+    int player2[5]{ 15, 15, 15, 15, 15 };
     int takenSquares[9]{ 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     int index1{ 0 };
     int index2{ 0 };
     int takenIndex{ 0 };
-    bool winningPlayer{ 0 };
 
     for (int i{ 0 }; i < 9; i++) {
         validInput = 1;
@@ -172,36 +151,26 @@ void game() {
             }
             else {
                 validInput = 1;
-            }
 
-            if (i % 2 == 0) {
-                player1[index1] = num;
-                takenSquares[takenIndex] = num;
+                if (i % 2 == 0) {
+                    player1[index1] = num;
+                    takenSquares[takenIndex] = num;
 
-                //swapGrid(num, 'X');
-                drawGrid();
+                    swapGrid(input, 'X');
+                    drawGrid();
+                    calculateWin(player1, 1);
+                }
+                else {
+                    player2[index2] = num;
+                    takenSquares[takenIndex] = num;
 
-                int win1 = calculateWin(player1);
-                if (win1 == 15) {
-                    i = 9;
-                    std::cout << "Player 1 has won!\n";
-                    winningPlayer = 1;
+                    swapGrid(input, 'O');
+                    drawGrid();
+                    calculateWin(player2, 2);
                 }
             }
-            else {
-                player2[index2] = num;
-                takenSquares[takenIndex] = num;
 
-                //swapGrid(num, 'O');
-                drawGrid();
-
-                int win2 = calculateWin(player2);
-                if (win2 == 15) {
-                    i = 9;
-                    std::cout << "Player 2 has won!\n";
-                    winningPlayer = 1;
-                }
-            }
+            
         } while (validInput != 1);
 
         index1++;
@@ -209,14 +178,9 @@ void game() {
         takenIndex++;
 
     }
-    if (winningPlayer == 0) {
-        std::cout << "It's a draw!\n";
-    }
+    std::cout << "It's a draw!";
     system("pause");
-}
-
-void gameVsAi() {
-
+    return;
 }
 
 void menu() {
@@ -236,9 +200,7 @@ void menu() {
         std::cout << "\t\t\t\t\t     |                     |\n";
         std::cout << "\t\t\t\t\t     | 1. Player VS Player |\n";
         std::cout << "\t\t\t\t\t     |                     |\n";
-        std::cout << "\t\t\t\t\t     | 2.   Player VS AI   |\n";
-        std::cout << "\t\t\t\t\t     |                     |\n";
-        std::cout << "\t\t\t\t\t     | 3.    Exit Game     |\n";
+        std::cout << "\t\t\t\t\t     | 2.    Exit Game     |\n";
         std::cout << "\t\t\t\t\t     |_____________________|\n\n\n";
 
         std::cin >> answer;
@@ -250,20 +212,16 @@ void menu() {
             break;
 
         case 2:
-            system("cls");
-            //gameVsAi();
-            break;
-
-        case 3:
             break;
 
         default:
             std::cout << "Invalid input\n";
 
         }
-    } while (answer != 3);
+    } while (answer != 2);
 }
 
 int main() {
     menu();
+    return 0;
 }
